3b45643e01ad08132889bf77bdd6fd29cabc1c12
Alex Wellnitz <moin@wellnitz-alex.de>
2020-06-16 22:32:24 +0200

    Initial Commit

---
+++ Planty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planty
+//
+//  Created by Alex Wellnitz on 16.06.20.
+//  Copyright © 2020 Wellcom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
